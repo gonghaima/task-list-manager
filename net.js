1ee8ecf5a3090e2a8a281584c292c10f3454d277
“Steven <steven.gong@iag.com.au>
2018-05-23 17:13:01 +1000

    Improving the chat server

--- net.js
+++ net.js
@@ -4,15 +4,27 @@ const server = require('net').createServer();
 let counter = 0;
 let sockets = {};
 
+function timestamp(){
+  const now = new Date();
+  return `${now.getHours()}:${now.getMinutes()}`;
+};
+
 server.on('connection', socket => {
   socket.id = counter++;
-  sockets[socket.id] = socket;
+
   console.log('Client connected');
-  socket.write('Welcome new client!\n');
+  socket.write('Please type your name: ');
 
   socket.on('data', data=>{
-    Object.entries(sockets).forEach(([, cs])=>{
-      cs.write(`${socket.id}: `);
+    if(!sockets[socket.id]){
+      socket.name = data.toString().trim();
+      socket.write(`Welcome ${socket.name}!\n`);
+      sockets[socket.id] = socket;
+      return;
+    }
+    Object.entries(sockets).forEach(([key, cs])=>{
+      if(socket.id==key)return;
+      cs.write(`${socket.name} ${timestamp()} :`);
       cs.write(data);
     });
   });
